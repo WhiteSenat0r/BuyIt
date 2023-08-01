c063cf1ce1e76c2578b810a59c27b582e5ea8a82
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-01 19:55:25 +0300

    Created IEntity interface

---
+++ Core/Common/Interfaces/IEntity.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Common.Interfaces;
+
+public interface IEntity<TKey> // Interface that represents a general template of entities with primary key
+    where TKey : struct
+{
+    [Key] public TKey Id { get; }
+}
