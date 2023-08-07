8cf473e69851923b335efb4ef29bc99303b8774c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-07 21:30:54 +0300

    Created RepositoryFactory.cs for the further creation of other factories

---
+++ Infrastructure/Repositories/Factories/Common/Classes/RepositoryFactory.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Repositories.Common.Interfaces;
+
+namespace Infrastructure.Repositories.Factories.Common.Classes;
+
+public abstract class RepositoryFactory<TEntity> 
+    where TEntity : class
+{
+    public abstract IRepository<TEntity> Create(StoreContext dbContext);
+}
