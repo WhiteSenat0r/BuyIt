fe80c7a62bdf78557a8e20ab10e0063df090ea04
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-07 21:23:04 +0300

    Added new declarations of methods in IRepository.cs

--- Infrastructure/Repositories/Common/Interfaces/IRepository.cs
+++ Infrastructure/Repositories/Common/Interfaces/IRepository.cs
@@ -1,16 +1,21 @@
-﻿#nullable enable
-namespace Infrastructure.Repositories.Common.Interfaces;
+﻿namespace Infrastructure.Repositories.Common.Interfaces;
 
 public interface IRepository<TEntity> 
     where TEntity : class
 {
-    Task<IEnumerable<TEntity>?> GetAllEntitiesAsync();
+    Task<IEnumerable<TEntity>> GetAllEntitiesAsync();
     
-    Task<TEntity?> GetSingleEntityAsync(Guid id);
+    Task<TEntity> GetSingleEntityAsync(Guid id);
 
     Task AddNewEntityAsync(TEntity entity);
     
+    Task AddNewRangeOfEntitiesAsync(IEnumerable<TEntity> entities);
+    
     void UpdateExistingEntity(TEntity updatedEntity);
     
+    void UpdateRangeOfExistingEntities(IEnumerable<TEntity> updatedEntities);
+    
     void RemoveExistingEntity(TEntity removedEntity);
+    
+    void RemoveRangeOfExistingEntities(IEnumerable<TEntity> removedEntities);
 }
