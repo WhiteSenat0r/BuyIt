4e95ab8f781f55cd6957ed74ae9f1785163de43b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-07 15:16:07 +0300

    Implemented generic IRepository.cs interface

---
+++ Infrastructure/Repositories/Common/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+namespace Infrastructure.Repositories.Common.Interfaces;
+
+public interface IRepository<TEntity> 
+    where TEntity : class
+{
+    Task<IEnumerable<TEntity>?> GetAllEntitiesAsync();
+    
+    Task<TEntity?> GetSingleEntityAsync(Guid id);
+
+    Task AddNewEntityAsync(TEntity entity);
+    
+    void UpdateExistingEntity(TEntity updatedEntity);
+    
+    void RemoveExistingEntity(TEntity removedEntity);
+}
