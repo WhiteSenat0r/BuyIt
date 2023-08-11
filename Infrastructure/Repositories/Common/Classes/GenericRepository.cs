b4ed0a811ef9f297e780c6ba05727aecdad12a06
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-11 13:17:41 +0300

    Removed delegates' properties and GetSingleEntityAsync method, changed realization of GetAllEntitiesAsync() method

--- Infrastructure/Repositories/Common/Classes/GenericRepository.cs
+++ Infrastructure/Repositories/Common/Classes/GenericRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Infrastructure.Contexts;
 using Infrastructure.Repositories.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories.Common.Classes;
 
@@ -11,22 +12,9 @@ public abstract class GenericRepository<TEntity> : IRepository<TEntity>
 
     private protected StoreContext Context { get; init; }
     
-    private protected Func<StoreContext, Task<List<TEntity>>> // Func delegate that is being used for invocation
-                                                              // in GetAllEntitiesAsync method as a compiled
-                                                              // query which returns all entities from the database 
-        AllEntitiesAsync { get; init; } = null!;
-
-    private protected Func<StoreContext, Guid, Task<TEntity>> // Func delegate that is being used for invocation
-                                                              // in GetSingleEntityAsync method as a compiled
-                                                              // query which returns a single entity from the database
-        SingleEntityAsync { get; init; } = null!;
-
     public async Task<IEnumerable<TEntity>> GetAllEntitiesAsync() => 
-        await AllEntitiesAsync(Context);
-    
-    public async Task<TEntity> GetSingleEntityAsync(Guid id) => 
-        await SingleEntityAsync(Context, id);
-    
+        await Context.Set<TEntity>().ToListAsync();
+
     public async Task AddNewEntityAsync(TEntity entity)
     {
         await Context.Set<TEntity>().AddAsync(entity);
