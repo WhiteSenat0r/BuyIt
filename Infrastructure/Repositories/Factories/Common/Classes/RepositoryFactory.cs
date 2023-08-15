26ac95a36875c39dca6a666567a5513053a14753
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-15 10:01:06 +0300

    Added realization of IEntity interface in RepositoryFactory.cs

--- Infrastructure/Repositories/Factories/Common/Classes/RepositoryFactory.cs
+++ Infrastructure/Repositories/Factories/Common/Classes/RepositoryFactory.cs
@@ -1,10 +1,11 @@
-﻿using Infrastructure.Contexts;
+﻿using Core.Common.Interfaces;
+using Infrastructure.Contexts;
 using Infrastructure.Repositories.Common.Classes;
 
 namespace Infrastructure.Repositories.Factories.Common.Classes;
 
 public abstract class RepositoryFactory<TEntity> 
-    where TEntity : class
+    where TEntity : class, IEntity<Guid>
 {
     public abstract GenericRepository<TEntity> Create(StoreContext dbContext);
 }
