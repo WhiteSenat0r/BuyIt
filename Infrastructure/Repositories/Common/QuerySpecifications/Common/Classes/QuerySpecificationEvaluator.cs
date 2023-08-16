fbb6ae32920e1bc5d033bbbe05fc8ca8bc43b20b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-16 20:17:17 +0300

    Added QuerySpecificationEvaluator.cs as a part of specification pattern

---
+++ Infrastructure/Repositories/Common/QuerySpecifications/Common/Classes/QuerySpecificationEvaluator.cs
@@ -0,0 +1,31 @@
+﻿using Core.Common.Interfaces;
+using Infrastructure.Repositories.Common.QuerySpecifications.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Common.QuerySpecifications.Common.Classes;
+
+public static class QuerySpecificationEvaluator 
+{
+    public static IQueryable<TEntity> GetQuerySpecifications<TEntity>
+        (IQueryable<TEntity> innerQueryable, IQuerySpecification<TEntity> querySpecification)
+        where TEntity : class, IEntity<Guid>
+    {
+        var queryable = innerQueryable;
+
+        if (querySpecification.Criteria is not null)
+            queryable = queryable.Where(querySpecification.Criteria);
+
+        queryable = querySpecification.IncludedExpressions.Aggregate
+            (queryable, (current, includeExpression) =>
+                current.Include(includeExpression));
+        
+        if (querySpecification.OrderByAscendingExpression is not null 
+            && querySpecification.OrderByDescendingExpression is null)
+            queryable = queryable.OrderBy(querySpecification.OrderByAscendingExpression);
+        else if (querySpecification.OrderByDescendingExpression is not null 
+                 && querySpecification.OrderByAscendingExpression is null)
+            queryable = queryable.OrderByDescending(querySpecification.OrderByDescendingExpression);
+
+        return queryable;
+    }
+}
