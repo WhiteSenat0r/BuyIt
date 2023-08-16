8ac1e7c9517b2965625bee26251845d71d9db6a5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-16 20:07:49 +0300

    Added product query specifications for the further implementation in repository

---
+++ Infrastructure/Repositories/ProductRelated/QuerySpecifications/ProductQueries/ProductQueryByManufacturerIdSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Core.Entities.Product;
+using Infrastructure.Repositories.Common.QuerySpecifications.Common.Classes;
+
+namespace Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries;
+
+public class ProductQueryByManufacturerIdSpecification : QuerySpecification<Product>
+{
+    public ProductQueryByManufacturerIdSpecification(Guid manufacturerId) 
+        : base(criteria => criteria.ManufacturerId == manufacturerId)
+    {
+        AddIncludeRange(new List<Expression<Func<Product, object>>>
+        {
+            p => p.Manufacturer,
+            p => p.ProductType,
+            p => p.Rating,
+        });
+        
+        AddOrderByAscending(p => p.Name);
+    }
+}
---
+++ Infrastructure/Repositories/ProductRelated/QuerySpecifications/ProductQueries/ProductQueryByRatingIdSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Core.Entities.Product;
+using Infrastructure.Repositories.Common.QuerySpecifications.Common.Classes;
+
+namespace Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries;
+
+public class ProductQueryByRatingIdSpecification : QuerySpecification<Product>
+{
+    public ProductQueryByRatingIdSpecification(Guid ratingId) 
+        : base(criteria => criteria.RatingId == ratingId)
+    {
+        AddIncludeRange(new List<Expression<Func<Product, object>>>
+        {
+            p => p.Manufacturer,
+            p => p.ProductType,
+            p => p.Rating,
+        });
+        
+        AddOrderByAscending(p => p.Name);
+    }
+}
