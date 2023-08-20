b3c366a1847aea73a237d182a9c3fade24c803bd
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-20 14:13:24 +0300

    Added RepositoryFactoryTests.cs

---
+++ Tests/Infrastructure.UnitTests/RepositoryRelatedTests/RepositoryFactoryRelatedTests/RepositoryFactoryTests.cs
@@ -0,0 +1,55 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Repositories.Factories.ProductRelated;
+using Infrastructure.Repositories.ProductRelated;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests.Infrastructure.UnitTests.RepositoryRelatedTests.RepositoryFactoryRelatedTests;
+
+public class RepositoryFactoryTests
+{
+    private StoreContext _dbContext = null!;
+    
+    [Fact]
+    public void ProductRepositoryFactoryClass_Should_CreateNewInstanceAfterCreateMethodWasInvoked()
+    {
+        _dbContext = new StoreContext(new DbContextOptions<StoreContext>());
+        
+        var repository = new ProductRepositoryFactory().Create(_dbContext);
+        
+        Assert.NotNull(repository);
+        Assert.IsType<ProductRepository>(repository);
+    }
+    
+    [Fact]
+    public void ProductManufacturerRepositoryFactoryClass_Should_CreateNewInstanceAfterCreateMethodWasInvoked()
+    {
+        _dbContext = new StoreContext(new DbContextOptions<StoreContext>());
+        
+        var repository = new ProductManufacturerRepositoryFactory().Create(_dbContext);
+        
+        Assert.NotNull(repository);
+        Assert.IsType<ProductManufacturerRepository>(repository);
+    }
+    
+    [Fact]
+    public void ProductTypeRepositoryFactoryClass_Should_CreateNewInstanceAfterCreateMethodWasInvoked()
+    {
+        _dbContext = new StoreContext(new DbContextOptions<StoreContext>());
+        
+        var repository = new ProductTypeRepositoryFactory().Create(_dbContext);
+        
+        Assert.NotNull(repository);
+        Assert.IsType<ProductTypeRepository>(repository);
+    }
+    
+    [Fact]
+    public void ProductRatingRepositoryFactoryClass_Should_CreateNewInstanceAfterCreateMethodWasInvoked()
+    {
+        _dbContext = new StoreContext(new DbContextOptions<StoreContext>());
+        
+        var repository = new ProductRatingRepositoryFactory().Create(_dbContext);
+        
+        Assert.NotNull(repository);
+        Assert.IsType<ProductRatingRepository>(repository);
+    }
+}
