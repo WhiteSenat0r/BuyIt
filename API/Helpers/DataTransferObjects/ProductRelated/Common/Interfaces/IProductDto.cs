13a501095b1fc83a1642189cbe22ef85f660fbbe
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-22 20:51:27 +0300

    Created IProductDto.cs interface for a better flexibility

---
+++ API/Helpers/DataTransferObjects/ProductRelated/Common/Interfaces/IProductDto.cs
@@ -0,0 +1,24 @@
+﻿namespace API.Helpers.DataTransferObjects.ProductRelated.Common.Interfaces;
+
+public interface IProductDto
+{
+    string Name { get; }
+
+    string Description { get; }
+    
+    decimal Price { get; }
+    
+    string InStock { get; }
+    
+    string Brand { get; }
+    
+    string Rating { get; }
+    
+    string Category { get; }
+    
+    string ProductCode { get; }
+    
+    IEnumerable<string> Images { get; }
+
+    IDictionary<string, IDictionary<string, string>> Specifications { get; }
+}
