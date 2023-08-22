d97d8574544a29884689bf96ac10bf0c876e662c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-22 20:56:13 +0300

    Added ProductUrlResolver.cs

---
+++ API/Helpers/Resolvers/ProductUrlResolver.cs
@@ -0,0 +1,19 @@
+﻿using API.Helpers.DataTransferObjects.ProductRelated.Common.Interfaces;
+using AutoMapper;
+using Core.Entities.Product.Common.Interfaces;
+
+namespace API.Helpers.Resolvers;
+
+public class ProductUrlResolver : IValueResolver<IProduct, IProductDto, IEnumerable<string>>
+{
+    private readonly IConfiguration _configuration;
+
+    public ProductUrlResolver
+        (IConfiguration configuration) => _configuration = configuration;
+
+    public IEnumerable<string> Resolve
+        (IProduct source, IProductDto destination, 
+            IEnumerable<string> destMember, ResolutionContext context) => 
+        source.MainImagesNames.Select
+            (path => _configuration["ApiUrl"] + path).ToList();
+}
