f5af5c4bc2468793b8730231fd48568f0bfc01d9
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-24 21:12:00 +0300

    Added ExceptionMiddleware.cs and ApiException.cs

---
+++ API/Middlewares/ExceptionHandlerMiddleware/Common/Classes/ApiException.cs
@@ -0,0 +1,11 @@
+﻿using API.Responses.Common.Classes;
+
+namespace API.Middlewares.ExceptionHandlerMiddleware.Common.Classes;
+
+public class ApiException : ApiResponse
+{
+    public ApiException(int responseCode, string responseMessage, string responseDetails)
+        : base(responseCode, responseMessage) => ResponseDetails = responseDetails;
+    
+    public string ResponseDetails { get; set; }
+}
---
+++ API/Middlewares/ExceptionHandlerMiddleware/ExceptionMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Text.Json;
+using API.Middlewares.ExceptionHandlerMiddleware.Common.Classes;
+using API.Responses.Common.Classes;
+using API.Responses.Common.Interfaces;
+
+namespace API.Middlewares.ExceptionHandlerMiddleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _environment;
+
+    public ExceptionMiddleware
+        (RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment environment)
+    {
+        _next = next;
+        _logger = logger;
+        _environment = environment;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = GetSuitableResponse(e);
+
+            var serializedLog = JsonSerializer.Serialize(response, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+
+            await httpContext.Response.WriteAsync(serializedLog);
+        }
+    }
+
+    private object GetSuitableResponse(Exception e) =>
+        _environment.IsDevelopment()
+            ? new ApiException
+                ((int)HttpStatusCode.InternalServerError, e.Message, e.StackTrace!)
+            : new ApiResponse
+                ((int)HttpStatusCode.InternalServerError, null);
+}
