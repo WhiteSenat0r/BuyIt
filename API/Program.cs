3757ac67c99d94bfcdbfa794ab91aa19e6a1ee9c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-24 21:15:24 +0300

    Added required services, controllers altered API's behaviour with validation errors and performed other changes

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,11 @@
+using System.Text.Json.Serialization;
+using API.Middlewares.ExceptionHandlerMiddleware;
+using API.Responses;
+using Core.Entities.Product;
 using Infrastructure.Contexts;
+using Infrastructure.Repositories.Common.Interfaces;
+using Infrastructure.Repositories.Factories.ProductRelated;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,9 +25,50 @@ builder.Services.AddDbContext<StoreContext>(option =>
         ("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IRepository<Product>>(provider =>
+    new ProductRepositoryFactory().Create(
+        provider.GetService<StoreContext>()!));
+
+builder.Services.AddScoped<IRepository<ProductType>>(provider =>
+    new ProductTypeRepositoryFactory().Create(
+        provider.GetService<StoreContext>()!));
+
+builder.Services.AddScoped<IRepository<ProductManufacturer>>(provider =>
+    new ProductManufacturerRepositoryFactory().Create(
+        provider.GetService<StoreContext>()!));
+
+builder.Services.AddScoped<IRepository<ProductRating>>(provider =>
+    new ProductRatingRepositoryFactory().Create(
+        provider.GetService<StoreContext>()!));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = actionContext =>
+    {
+        var errors = actionContext.ModelState
+            .Where(e => e.Value.Errors.Count > 0)
+            .SelectMany(e => e.Value.Errors)
+            .Select(e => e.ErrorMessage).ToList();
+
+        var errorResponse = new ApiValidationErrorResponse(null)
+        {
+            Errors = errors
+        };
+
+        return new BadRequestObjectResult(errorResponse);
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,5 +78,20 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+await using var scope =  app.Services.CreateAsyncScope();
+
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    await context.Database.MigrateAsync();
+}
+catch (OperationCanceledException e)
+{
+    logger.LogError(e, "An error occured during migration!");
+}
 
 app.Run();
