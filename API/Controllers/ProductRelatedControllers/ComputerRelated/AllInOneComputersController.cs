5386ea371723a3dc8bfd347ccdab5e33a38d2a98
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-27 23:11:12 +0300

    Added computer-related product controllers

---
+++ API/Controllers/ProductRelatedControllers/ComputerRelated/AllInOneComputersController.cs
@@ -0,0 +1,20 @@
+﻿using API.Controllers.ProductRelatedControllers.Common.Classes;
+using AutoMapper;
+using Infrastructure.Contexts;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels.ComputerRelated;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.ComputerRelated;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.ProductRelatedControllers.ComputerRelated;
+
+[ApiController]
+public class AllInOneComputersController : BaseProductController
+    <AioComputerFilteringModel, AioComputerQuerySpecification>
+{
+    public AllInOneComputersController
+        (StoreContext storeContext, IMapper mapper) : base(storeContext, mapper)
+    {
+        Context = storeContext;
+        Mapper = mapper;
+    }
+}
---
+++ API/Controllers/ProductRelatedControllers/ComputerRelated/PersonalComputersController.cs
@@ -0,0 +1,20 @@
+﻿using API.Controllers.ProductRelatedControllers.Common.Classes;
+using AutoMapper;
+using Infrastructure.Contexts;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels.ComputerRelated;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.ComputerRelated;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.ProductRelatedControllers.ComputerRelated;
+
+[ApiController]
+public class PersonalComputersController : BaseProductController
+    <PersonalComputerFilteringModel, PersonalComputerQuerySpecification>
+{
+    public PersonalComputersController
+        (StoreContext storeContext, IMapper mapper) : base(storeContext, mapper)
+    {
+        Context = storeContext;
+        Mapper = mapper;
+    }
+}
