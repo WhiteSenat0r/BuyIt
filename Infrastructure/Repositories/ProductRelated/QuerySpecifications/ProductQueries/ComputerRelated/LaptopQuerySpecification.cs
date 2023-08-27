19f8d80cdfa5ab7dbba3c7327c5419e011fe5702
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-27 22:16:04 +0300

    Added computer-related query specifications

---
+++ Infrastructure/Repositories/ProductRelated/QuerySpecifications/ProductQueries/ComputerRelated/LaptopQuerySpecification.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels
+    .ComputerRelated;
+
+namespace Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.ComputerRelated;
+
+public class LaptopQuerySpecification : PersonalComputerQuerySpecification
+{
+    public LaptopQuerySpecification(LaptopFilteringModel filteringModel) : base(filteringModel) =>
+        SpecificationCriteria = SpecificationCriteria.And(
+            product => GetSpecificationCategoryFilteringModel("General", new[] { "Model family" },
+                           new[] { filteringModel.ModelFamily }, product) &&
+                       GetSpecificationCategoryFilteringModel("Display",
+                           new[] { "Diagonal", "Resolution", "Matrix type", "Refresh rate" },
+                           new[]
+                           {
+                               filteringModel.DisplayDiagonal, filteringModel.DisplayResolution,
+                               filteringModel.DisplayMatrixType, filteringModel.DisplayRefreshRate
+                           }, product));
+}
