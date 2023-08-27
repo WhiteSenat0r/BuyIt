0f84010bd31083a836a8705d68c05fbbcb5af60d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-27 23:11:38 +0300

    Updated code structure in Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,69 +1,13 @@
-using System.Text.Json.Serialization;
+using API.Extensions;
 using API.Middlewares.ExceptionHandlerMiddleware;
-using API.Responses;
-using Core.Entities.Product;
 using Infrastructure.Contexts;
-using Infrastructure.Repositories.Common.Interfaces;
-using Infrastructure.Repositories.Factories.ProductRelated;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddControllers().AddJsonOptions(x =>
-    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(option =>
-{
-    option.UseSqlServer(builder.Configuration.GetConnectionString
-        ("DefaultConnection"));
-});
-
-builder.Services.AddScoped<IRepository<Product>>(provider =>
-    new ProductRepositoryFactory().Create(
-        provider.GetService<StoreContext>()!));
-
-builder.Services.AddScoped<IRepository<ProductType>>(provider =>
-    new ProductTypeRepositoryFactory().Create(
-        provider.GetService<StoreContext>()!));
-
-builder.Services.AddScoped<IRepository<ProductManufacturer>>(provider =>
-    new ProductManufacturerRepositoryFactory().Create(
-        provider.GetService<StoreContext>()!));
-
-builder.Services.AddScoped<IRepository<ProductRating>>(provider =>
-    new ProductRatingRepositoryFactory().Create(
-        provider.GetService<StoreContext>()!));
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = actionContext =>
-    {
-        var errors = actionContext.ModelState
-            .Where(e => e.Value.Errors.Count > 0)
-            .SelectMany(e => e.Value.Errors)
-            .Select(e => e.ErrorMessage).ToList();
-
-        var errorResponse = new ApiValidationErrorResponse(null)
-        {
-            Errors = errors
-        };
-
-        return new BadRequestObjectResult(errorResponse);
-    };
-});
+builder.Services.AddRequiredApplicationServiceCollection(builder.Configuration);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
