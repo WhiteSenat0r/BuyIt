cdd5942b415d75eddb1dc63c9d3760cf984caceb
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-27 19:50:20 +0300

    Added BasicProductFilteringModel.cs

---
+++ Infrastructure/Repositories/ProductRelated/QuerySpecifications/ProductQueries/Common/FilteringModels/Common/Classes/BasicProductFilteringModel.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common
+    .FilteringModels.Common.Interfaces;
+
+namespace Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common
+    .FilteringModels.Common.Classes;
+
+public abstract class BasicProductFilteringModel : IProductFilteringModel
+{
+    public string BrandName { get; set; }
+    
+    public decimal? LowerPriceLimit { get; set; }
+    
+    public decimal? UpperPriceLimit { get; set; }
+    
+    public string SortingType { get; set; }
+}
