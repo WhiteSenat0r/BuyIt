38fb9006d30aecb1e50fe67acc5bfbc769d69ecd
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-27 23:07:14 +0300

    Added generic type parameters and Get method in BaseProductController.cs

--- API/Controllers/ProductRelatedControllers/Common/Classes/BaseProductController.cs
+++ API/Controllers/ProductRelatedControllers/Common/Classes/BaseProductController.cs
@@ -6,36 +6,57 @@ using AutoMapper;
 using Infrastructure.Contexts;
 using Infrastructure.Repositories.Factories.ProductRelated;
 using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.Classes;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels
+    .Common.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Controllers.ProductRelatedControllers.Common.Classes;
 
-public class BaseProductController : BaseApiController
+public abstract class BaseProductController<TFilteringModel, TQuerySpecification> : BaseApiController
+    where TFilteringModel : IProductFilteringModel
+    where TQuerySpecification : BasicProductQuerySpecification
 {
-    private readonly StoreContext _context;
-    private readonly IMapper _mapper;
-
-    public BaseProductController
+    protected BaseProductController
         (StoreContext storeContext, IMapper mapper)
     {
-        _context = storeContext;
-        _mapper = mapper;
+        Context = storeContext;
+        Mapper = mapper;
     }
 
-    protected string Category { get; init; } = null!;
+    protected StoreContext Context { get; init; }
+    
+    protected IMapper Mapper { get; init; }
 
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<IEnumerable<IProductDto>>> Get([FromQuery] TFilteringModel filteringModel)
+    {
+        var specification = (TQuerySpecification)Activator
+            .CreateInstance(typeof(TQuerySpecification), filteringModel);
+        
+        var items = (await new ProductRepositoryFactory().Create(Context)
+            .GetAllEntitiesAsync(specification)).Where(specification!.SpecificationCriteria.Compile());
+
+        return items.IsNullOrEmpty()
+            ? NotFound(new ApiResponse(404, @"No products were found!"))
+            : Ok(Mapper.Map<IEnumerable<ProductDto>>(items));
+    }
+    
     [HttpGet("[controller]/item/{productCode}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<IProductDto>> GetSingleByCode(string productCode)
+    public async Task<ActionResult<IProductDto>> GetSingle(string productCode)
     {
-        var item = await new ProductRepositoryFactory().Create(_context)
+        var item = await new ProductRepositoryFactory().Create(Context)
             .GetSingleEntityBySpecificationAsync
                 (new ProductQueryByProductCodeSpecification(productCode));
 
         return item is null
             ? NotFound(new ApiResponse(404, 
                 $"Item with given product code ({productCode}) was not found!"))
-            : Ok(_mapper.Map<ProductDto>(item));
+            : Ok(Mapper.Map<ProductDto>(item));
     }
 }
