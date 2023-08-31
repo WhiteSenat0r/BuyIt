1f14484d177767c632575b947ef59de1bd291254
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-31 18:32:07 +0300

    Added new property declarations in IQuerySpecification.cs and their implementation in

--- Infrastructure/Repositories/Common/QuerySpecifications/Common/Classes/QuerySpecification.cs
+++ Infrastructure/Repositories/Common/QuerySpecifications/Common/Classes/QuerySpecification.cs
@@ -9,15 +9,23 @@ public abstract class QuerySpecification<TEntity> : IQuerySpecification<TEntity>
 {
     protected QuerySpecification() { }
     
-    protected QuerySpecification(Expression<Func<TEntity, bool>> criteria) => Criteria = criteria;
+    protected QuerySpecification(Expression<Func<TEntity, bool>> mainCriteria) => Criteria = mainCriteria;
 
-    public Expression<Func<TEntity, bool>> Criteria { get; }
+    public Expression<Func<TEntity, bool>> Criteria { get; init; }
+    
+    public Expression<Func<TEntity, bool>> SpecificationCriteria { get; init; }
 
     public List<Expression<Func<TEntity, object>>> IncludedExpressions { get; } = new();
     
     public Expression<Func<TEntity, object>> OrderByAscendingExpression { get; private set; }
     
     public Expression<Func<TEntity, object>> OrderByDescendingExpression { get; private set; }
+    
+    public int SkippedItemsQuantity { get; private set; }
+    
+    public int TakenItemsQuantity { get; private set; }
+    
+    public bool IsPagingEnabled { get; private set; }
 
     protected void AddInclude(Expression<Func<TEntity, object>> includedExpression)
         => IncludedExpressions.Add(includedExpression);
@@ -25,6 +33,13 @@ public abstract class QuerySpecification<TEntity> : IQuerySpecification<TEntity>
     protected void AddIncludeRange
         (IEnumerable<Expression<Func<TEntity, object>>> includedExpressions) =>
         IncludedExpressions.AddRange(includedExpressions);
+
+    protected void AddPaging(int takenItemsQuantity, int skippedItemsQuantity)
+    {
+        TakenItemsQuantity = takenItemsQuantity;
+        SkippedItemsQuantity = skippedItemsQuantity;
+        IsPagingEnabled = true;
+    }
 
     protected void AddOrderByAscending(Expression<Func<TEntity, object>> orderByAscendingExpression)
         => OrderByAscendingExpression = orderByAscendingExpression;
--- Infrastructure/Repositories/Common/QuerySpecifications/Common/Interfaces/IQuerySpecification.cs
+++ Infrastructure/Repositories/Common/QuerySpecifications/Common/Interfaces/IQuerySpecification.cs
@@ -1,16 +1,23 @@
 ﻿using System.Linq.Expressions;
-using Core.Common.Interfaces;
 
 namespace Infrastructure.Repositories.Common.QuerySpecifications.Common.Interfaces;
 
 public interface IQuerySpecification<TEntity> 
-    where TEntity : class, IEntity<Guid>
+    where TEntity : class
 {
     Expression<Func<TEntity, bool>> Criteria { get; }
+    
+    Expression<Func<TEntity, bool>> SpecificationCriteria { get; protected init; }
 
     List<Expression<Func<TEntity, object>>> IncludedExpressions { get; } 
     
     Expression<Func<TEntity, object>> OrderByAscendingExpression { get; }
     
     Expression<Func<TEntity, object>> OrderByDescendingExpression { get; }
+    
+    int SkippedItemsQuantity { get; }
+    
+    int TakenItemsQuantity { get; }
+    
+    bool IsPagingEnabled { get; }
 }
