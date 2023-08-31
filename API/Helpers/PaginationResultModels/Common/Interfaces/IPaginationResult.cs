4d24f030ee02fe4798405789071068bd0b6d079d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-08-31 18:29:57 +0300

    Added IPaginationResult.cs

---
+++ API/Helpers/PaginationResultModels/Common/Interfaces/IPaginationResult.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Product.Common.Interfaces;
+
+namespace API.Helpers.PaginationResultModels.Common.Interfaces;
+
+public interface IPaginationResult<TEntity> where TEntity : class
+{
+    IEnumerable<IProduct> Items { get; }
+    
+    int ItemsQuantity { get; }
+    
+    int PageIndex { get; }
+    
+    int PageSize { get; }
+}
