f8cf3119209b0865a0952718e4d38a84316b1ef6
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-02 21:36:18 +0300

    Removed "PageSize" property from ProductPaginationResult.cs.cs and its declaration in IPaginationResult.cs

--- API/Helpers/PaginationResultModels/Common/Interfaces/IPaginationResult.cs
+++ API/Helpers/PaginationResultModels/Common/Interfaces/IPaginationResult.cs
@@ -1,14 +1,12 @@
-﻿using Core.Entities.Product.Common.Interfaces;
+﻿using API.Helpers.DataTransferObjects.ProductRelated.Common.Interfaces;
 
 namespace API.Helpers.PaginationResultModels.Common.Interfaces;
 
 public interface IPaginationResult<TEntity> where TEntity : class
 {
-    IEnumerable<IProduct> Items { get; }
+    IEnumerable<IProductDto> Items { get; }
     
     int ItemsQuantity { get; }
     
     int PageIndex { get; }
-    
-    int PageSize { get; }
 }
--- API/Helpers/PaginationResultModels/ProductPaginationResult.cs
+++ API/Helpers/PaginationResultModels/ProductPaginationResult.cs
@@ -1,6 +1,6 @@
-﻿using API.Helpers.PaginationResultModels.Common.Interfaces;
+﻿using API.Helpers.DataTransferObjects.ProductRelated.Common.Interfaces;
+using API.Helpers.PaginationResultModels.Common.Interfaces;
 using Core.Entities.Product;
-using Core.Entities.Product.Common.Interfaces;
 using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels.Common.Classes;
 using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels.Common.Interfaces;
 
@@ -9,19 +9,16 @@ namespace API.Helpers.PaginationResultModels;
 public class ProductPaginationResult : IPaginationResult<Product>
 {
     public ProductPaginationResult
-        (IEnumerable<IProduct> items, IProductFilteringModel filteringModel)
+        (IEnumerable<IProductDto> items, IProductFilteringModel filteringModel)
     {
         Items = items;
         ItemsQuantity = Items.Count();
         PageIndex = filteringModel.PageIndex;
-        PageSize = BasicProductFilteringModel.MaximumItemQuantity;
     }
     
-    public IEnumerable<IProduct> Items { get; }
+    public IEnumerable<IProductDto> Items { get; }
     
     public int ItemsQuantity { get; }
     
     public int PageIndex { get; }
-
-    public int PageSize { get; }
 }
