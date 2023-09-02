77c664de0af94507d44d29c7f51b756c8ba712b4
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-02 21:15:37 +0300

    Added IProductSpecification.cs and ProductSpecification.cs

---
+++ Core/Entities/Product/Common/Interfaces/IProductSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Core.Common.Interfaces;
+
+namespace Core.Entities.Product.Common.Interfaces;
+
+public interface IProductSpecification : IEntity<Guid>
+{
+    string Category { get; }
+    
+    string Attribute { get; }
+    
+    string Value { get; }
+}
---
+++ Core/Entities/Product/ProductSpecification.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Entities.Product.Common.Interfaces;
+
+namespace Core.Entities.Product;
+
+public class ProductSpecification : IProductSpecification
+{
+    public ProductSpecification() { }
+
+    public ProductSpecification
+        (string category, string attribute, string value, Guid productId)
+    {
+        Category = category;
+        Attribute = attribute;
+        Value = value;
+        ProductId = productId;
+    }
+
+    public Guid Id { get; }
+    
+    [MaxLength(48)]
+    public string Category { get; set; }
+
+    [MaxLength(48)]
+    public string Attribute { get; set; } 
+
+    [MaxLength(192)]
+    public string Value { get; set; }
+
+    public Guid ProductId { get; set; }
+    
+    public Product Product { get; set; } = null!;
+}
