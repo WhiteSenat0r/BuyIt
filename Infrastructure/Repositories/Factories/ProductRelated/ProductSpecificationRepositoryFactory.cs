169ac22361056fa924552487b802545200a6ebfa
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-02 21:27:45 +0300

    Added ProductSpecificationRepository.cs and ProductSpecificationRepositoryFactory.cs

---
+++ Infrastructure/Repositories/Factories/ProductRelated/ProductSpecificationRepositoryFactory.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Product;
+using Infrastructure.Contexts;
+using Infrastructure.Repositories.Factories.Common.Classes;
+using Infrastructure.Repositories.ProductRelated;
+
+namespace Infrastructure.Repositories.Factories.ProductRelated;
+
+public class ProductSpecificationRepositoryFactory : RepositoryFactory<ProductSpecification>
+{
+    public override ProductSpecificationRepository Create(StoreContext dbContext) =>  new(dbContext);
+}
---
+++ Infrastructure/Repositories/ProductRelated/ProductSpecificationRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Product;
+using Infrastructure.Contexts;
+using Infrastructure.Repositories.Common.Classes;
+
+namespace Infrastructure.Repositories.ProductRelated;
+
+public sealed class ProductSpecificationRepository : GenericRepository<ProductSpecification>
+{
+    internal ProductSpecificationRepository
+        (StoreContext dbContext) : base(dbContext) => Context = dbContext;
+}
