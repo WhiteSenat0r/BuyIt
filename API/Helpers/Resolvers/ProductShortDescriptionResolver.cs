2f727c386309567d684b7cd46aa69588254348a8
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-03 21:56:51 +0300

    Added ProductShortDescriptionResolver.cs and ProductSpecificationResolver.cs

---
+++ API/Helpers/Resolvers/ProductShortDescriptionResolver.cs
@@ -0,0 +1,19 @@
+﻿using API.Helpers.DataTransferObjects.ProductRelated.Common.Interfaces;
+using API.Helpers.Resolvers.ShortDescriptionResolver.Common.Classes;
+using AutoMapper;
+using Core.Entities.Product.Common.Interfaces;
+
+namespace API.Helpers.Resolvers;
+
+public class ProductShortDescriptionResolver : IValueResolver<IProduct, IProductDto, string>
+{
+    public string Resolve
+        (IProduct source, IProductDto destination, string destMember, ResolutionContext context) =>
+        source.ProductType.Name switch
+        {
+            "Personal computer" or "All-in-one computer" or "Laptop" =>
+                new ComputerShortDescription().GetShortDescription(source),
+            _ => throw new ArgumentException
+                ("Short description can not be received due to unknown product type!")
+        };
+}
---
+++ API/Helpers/Resolvers/ProductSpecificationResolver.cs
@@ -0,0 +1,29 @@
+﻿using API.Helpers.DataTransferObjects.ProductRelated;
+using AutoMapper;
+using Core.Entities.Product.Common.Interfaces;
+
+namespace API.Helpers.Resolvers;
+
+public class ProductSpecificationResolver : IValueResolver<IProduct, FullProductDto, IDictionary<string, IDictionary<string, string>>>
+{
+    public IDictionary<string, IDictionary<string, string>> Resolve
+        (IProduct source, FullProductDto destination, 
+            IDictionary<string, IDictionary<string, string>> destMember, ResolutionContext context)
+    {
+        var categories = source.Specifications
+            .DistinctBy(s => s.Category).Select(c => c.Category);
+
+        IDictionary<string, IDictionary<string, string>> result = new Dictionary<string, IDictionary<string, string>>();
+
+        foreach (var category in categories)
+        {
+            var attributesAndValues = 
+                source.Specifications.Where(c => c.Category.Equals(category))
+                    .ToDictionary(specification => specification.Attribute, specification => specification.Value);
+
+            result.Add(category, attributesAndValues);
+        }
+
+        return result;
+    }
+}
