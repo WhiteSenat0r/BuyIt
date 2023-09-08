f474413565fb89b5248be513a528cbdffe199562
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-08 20:15:23 +0300

    Created product search-related filtering model and query specification

---
+++ Infrastructure/Repositories/ProductRelated/QuerySpecifications/ProductQueries/Common/FilteringModels/ProductSearchFilteringModel.cs
@@ -0,0 +1,8 @@
+﻿using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels.Common.Classes;
+
+namespace Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels;
+
+public class ProductSearchFilteringModel : BasicFilteringModel
+{
+    public string Text { get; set; }
+}
