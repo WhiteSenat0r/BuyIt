c47c18fd3c1dd893f8225472eee7edba2028d930
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-08 20:19:20 +0300

    Added ProductSearchController.cs

---
+++ API/Controllers/ProductRelatedControllers/Common/Classes/ProductSearchController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.Entities.Product;
+using Infrastructure.Repositories.Common.Interfaces;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.Classes;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.ProductRelatedControllers.Common.Classes;
+
+[ApiController]
+public class ProductSearchController : BaseProductRelatedController
+    <ProductSearchFilteringModel, ProductSearchQuerySpecification>
+{
+    public ProductSearchController(IRepository<Product> products, IMapper mapper) : base(products, mapper) { }
+}
