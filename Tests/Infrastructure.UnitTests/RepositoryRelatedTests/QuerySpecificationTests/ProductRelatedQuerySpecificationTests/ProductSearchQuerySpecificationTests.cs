e58ea98d796d81351dcc2578633e202153f8e1af
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-08 20:23:27 +0300

    Added ProductSearchQuerySpecificationTests.cs

---
+++ Tests/Infrastructure.UnitTests/RepositoryRelatedTests/QuerySpecificationTests/ProductRelatedQuerySpecificationTests/ProductSearchQuerySpecificationTests.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Product;
+using Infrastructure.Repositories.Common.QuerySpecifications.Common.Interfaces;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.Classes;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels;
+
+namespace Tests.Infrastructure.UnitTests.RepositoryRelatedTests.QuerySpecificationTests.ProductRelatedQuerySpecificationTests;
+
+public class ProductSearchQuerySpecificationTests
+{
+    private IQuerySpecification<Product> _querySpecification = null!;
+
+    [Fact]
+    public void ProductQuerySpecificationConstructor_Should_CreateNewQuerySpecificationInstance()
+    {
+        _querySpecification = new ProductSearchQuerySpecification(new ProductSearchFilteringModel());
+        
+        Assert.IsType<ProductSearchQuerySpecification>(_querySpecification);
+        Assert.NotNull(_querySpecification);
+    }
+}
