058c5591f21e4506a5eb95ede174108fd8628300
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-17 23:30:34 +0300

    Added ProductSpecificationAspectQuerySpecification.cs and tests for it

---
+++ Infrastructure/Repositories/ProductRelated/QuerySpecifications/ProductQueries/RegularSpecifications/ProductSpecificationRelated/ProductSpecificationAspectQuerySpecification.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using Core.Entities.Product.ProductSpecification.Common.Interfaces;
+using Infrastructure.Repositories.Common.QuerySpecifications.Common.Classes;
+
+namespace Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.RegularSpecifications.ProductSpecificationRelated;
+
+public sealed class ProductSpecificationAspectQuerySpecification : QuerySpecification<ISpecificationAspect>
+{
+    public ProductSpecificationAspectQuerySpecification(Expression<Func<ISpecificationAspect, bool>> criteria)
+        : base(criteria) => AddOrderByAscending(p => p.Value);
+}
---
+++ Tests/Infrastructure.UnitTests/RepositoryRelatedTests/QuerySpecificationTests/ProductRelatedQuerySpecificationTests/ProductSpecificationAspectQuerySpecificationTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using Core.Entities.Product.ProductSpecification.Common.Interfaces;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.RegularSpecifications.ProductSpecificationRelated;
+
+namespace Tests.Infrastructure.UnitTests.RepositoryRelatedTests.QuerySpecificationTests.ProductRelatedQuerySpecificationTests;
+
+public class ProductSpecificationAspectQuerySpecificationTests
+{
+    [Fact]
+    public void Constructor_ShouldSetCriteriaAndOrderByExpression()
+    {
+        Expression<Func<ISpecificationAspect, bool>> criteria = aspect => aspect.Value == "Test";
+        
+        var specification = new ProductSpecificationAspectQuerySpecification(criteria);
+        
+        Assert.Equal(criteria, specification.Criteria);
+    }
+}
