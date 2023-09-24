dc8356424878967266cd76440f6899882883040a
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-24 10:03:14 +0300

    Updated database after "Manufacturer" entity changes

---
+++ Infrastructure/Data/Migrations/20230924065611_UpdatedManufacturerEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedManufacturerEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationCountry",
+                table: "ProductManufacturers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RegistrationCountry",
+                table: "ProductManufacturers",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: true);
+        }
+    }
+}
