ac6ab9761207fa2309beebdec78d0c1ee1eba647
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-09-29 20:17:19 +0300

    Added "GetAllCountedCategoryRelatedManufacturers" method in ProductManufacturerRepository.cs

--- Infrastructure/Repositories/ProductRelated/ProductManufacturerRepository.cs
+++ Infrastructure/Repositories/ProductRelated/ProductManufacturerRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Core.Entities.Product;
 using Infrastructure.Contexts;
 using Infrastructure.Repositories.Common.Classes;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.FilteringModels.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories.ProductRelated;
 
@@ -8,4 +10,16 @@ public sealed class ProductManufacturerRepository : GenericRepository<ProductMan
 {
     internal ProductManufacturerRepository
         (StoreContext dbContext) : base(dbContext) => Context = dbContext;
+
+    public async Task<Dictionary<string, int>> GetAllCountedCategoryRelatedManufacturers(
+        IFilteringModel filteringModel)
+    {
+        var productManufacturers = await Context.ProductManufacturers.ToListAsync();
+
+        return productManufacturers.ToDictionary(
+            brand => brand.Name,
+            brand => Context.Products
+                .Where(product => product.ProductType.Name.Equals(filteringModel.Category.First()))
+                .Count(product => product.Manufacturer.Name.Equals(brand.Name)));
+    }
 }
