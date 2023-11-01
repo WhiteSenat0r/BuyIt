f7ff0d5a4d1ca5aeaacbb041ec8c6b07a95df6b4
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-11-01 19:12:20 +0200

    Extended ProductQuerySpecification.cs with a new constructor

--- Infrastructure/Repositories/ProductRelated/QuerySpecifications/ProductQueries/RegularSpecifications/ProductQuerySpecification.cs
+++ Infrastructure/Repositories/ProductRelated/QuerySpecifications/ProductQueries/RegularSpecifications/ProductQuerySpecification.cs
@@ -1,8 +1,13 @@
-﻿using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.Classes;
+﻿using System.Linq.Expressions;
+using Core.Entities.Product;
+using Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.Common.Classes;
 
 namespace Infrastructure.Repositories.ProductRelated.QuerySpecifications.ProductQueries.RegularSpecifications;
 
 public sealed class ProductQuerySpecification : BasicProductQuerySpecification
 {
     public ProductQuerySpecification() => AddOrderByAscending(p => p.Name);
+
+    public ProductQuerySpecification(Expression<Func<Product, bool>> expression) : base(expression) 
+        => AddOrderByAscending(p => p.Name);
 }
