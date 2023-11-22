ce368195bc495803791f094d48407f2e6c8b0ec3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-11-22 17:06:22 +0200

    Added classes with constant attributes and categories of the specifications

---
+++ BuyIt.Core.Application/Helpers/SpecificationResolver/Common/Constants/FilterAttributeConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Helpers.SpecificationResolver.Common.Constants;
+
+public sealed class FilterAttributeConstants
+{
+    public IEnumerable<string> RemovedAttributes { get; } = new []
+    {
+        "Base clock", "Max clock", "Processor technology", 
+        "Quantity of threads", "Type of memory", "Memory bus",
+        "Drive's interface"
+    };
+}
---
+++ BuyIt.Core.Application/Helpers/SpecificationResolver/Common/Constants/FilterCategoryConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Helpers.SpecificationResolver.Common.Constants;
+
+public sealed class FilterCategoryConstants
+{
+    public IEnumerable<string> RemovedCategories { get; } = new []
+    {
+        "Measurements", "Interfaces and connection"
+    };
+}
