e435fda82770d824c2951703c604feb63dd2180e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-11-22 17:06:45 +0200

    Added ProductFilterCounter.cs

---
+++ BuyIt.Core.Application/Helpers/SpecificationResolver/Common/ProductFilterCounter.cs
@@ -0,0 +1,170 @@
+﻿using System.Reflection;
+using Application.Contracts;
+using Application.DataTransferObjects.ProductRelated.Specification;
+using Application.FilteringModels;
+using Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Helpers.SpecificationResolver.Common;
+
+public sealed class ProductFilterCounter
+{
+    private readonly IEnumerable<Product> _categoryRelatedProducts;
+    private readonly IEnumerable<Product> _filteredProducts;
+    private readonly IEnumerable<Product> _brandlessFilteredProducts;
+    private readonly IEnumerable<ProductSpecification> _commonSpecifications;
+    private readonly IEnumerable<ProductManufacturer> _manufacturers;
+    private readonly IEnumerable<ProductType> _categories;
+    private readonly IFilteringModel _filteringModel;
+    private readonly ProductSpecificationExtractor _specificationExtractor;
+
+    public ProductFilterCounter(IEnumerable<Product> categoryRelatedProducts,
+        IEnumerable<Product> filteredProducts,
+        IEnumerable<Product> brandlessFilteredProducts,
+        IEnumerable<ProductSpecification> commonSpecifications,
+        IEnumerable<ProductManufacturer> manufacturers,
+        IEnumerable<ProductType> categories,
+        IFilteringModel filteringModel,
+        ProductSpecificationExtractor specificationExtractor)
+    {
+        _categoryRelatedProducts = categoryRelatedProducts;
+        _filteredProducts = filteredProducts;
+        _manufacturers = manufacturers;
+        _categories = categories;
+        _filteringModel = filteringModel;
+        _specificationExtractor = specificationExtractor;
+        _brandlessFilteredProducts = brandlessFilteredProducts;
+        _commonSpecifications = commonSpecifications;
+    }
+    
+    public IDictionary<string, int> GetCountedSpecifications()
+    {
+        var countedSpecs = new Dictionary<string, int>();
+    
+        if (_filteringModel is ProductSearchFilteringModel)
+            return countedSpecs;
+    
+        var extractedSpecification = _specificationExtractor.ExtractSpecificationsForCounting(
+            _commonSpecifications);
+        
+        CountSpecifications(
+            extractedSpecification, countedSpecs, _filteredProducts, _categoryRelatedProducts);
+    
+        return countedSpecs;
+    }
+    
+    public IDictionary<string, int> GetCountedBrands()
+    {
+        var filterListsDictionary = GetFilterListsDictionary(_filteringModel, 
+            _filteringModel.GetType().GetProperties().Where(
+                info => info.PropertyType == typeof(List<string>)));
+
+        if ((IsSatisfyingProductModelPredicate(_filteringModel, filterListsDictionary) ||
+             IsSatisfyingSearchModelPredicate(_filteringModel, filterListsDictionary))
+            && IsWithoutPriceLimits(_filteringModel))
+            return RemoveZeroCountsFromDictionary(
+                GetAllCountedCategoryRelatedManufacturersAsync());
+
+        return RemoveZeroCountsFromDictionary(GetCountedBrandsToDictionary());
+    }
+    
+    public IDictionary<string, int> GetCountedCategories() =>
+        _filteringModel.GetType() != typeof(ProductSearchFilteringModel) 
+            ? new Dictionary<string, int>() 
+            : GetCountedCategoriesToDictionary();
+
+    private IDictionary<string, int> GetCountedCategoriesToDictionary() =>
+        _categories
+        .ToDictionary(
+            category => category.Name,
+            category =>
+            {
+                return _filteredProducts.Count(
+                    product => product.ProductType.Name == category.Name);
+            });
+    
+    private IDictionary<string, int> GetAllCountedCategoryRelatedManufacturersAsync()
+    {
+        if (_filteringModel.GetType() != typeof(ProductSearchFilteringModel))
+        {
+            return _manufacturers.ToDictionary(
+                brand => brand.Name,
+                brand => _categoryRelatedProducts
+                    .Count(product => product.Manufacturer.Name.Equals(brand.Name)));
+        }
+        
+        return _manufacturers.ToDictionary(
+            brand => brand.Name,
+            brand => _filteredProducts
+                .Count(product => product.Manufacturer.Name.Equals(brand.Name)));
+    }
+    
+    private IDictionary<string, int> GetCountedBrandsToDictionary()
+    {
+        var result = new Dictionary<string, int>();
+
+        foreach (var product in _brandlessFilteredProducts.Where(
+                     product => !result.ContainsKey(product.Manufacturer.Name)))
+            result.Add(product.Manufacturer.Name, _brandlessFilteredProducts.Count(selectedProduct =>
+                selectedProduct.Manufacturer.Name == product.Manufacturer.Name));
+
+        return result;
+    }
+    
+    private IDictionary<string, List<string>> GetFilterListsDictionary(
+        IFilteringModel filteringModel, IEnumerable<PropertyInfo> filterProperties) =>
+        filterProperties.ToDictionary(
+            key => key.Name,
+            value => (List<string>)value.GetValue(filteringModel));
+    
+    private bool IsWithoutPriceLimits(IFilteringModel filteringModel) =>
+        filteringModel.UpperPriceLimit is null
+        && filteringModel.LowerPriceLimit is null;
+
+    private bool IsSatisfyingProductModelPredicate(IFilteringModel filteringModel, 
+        IDictionary<string, List<string>> filterListsDictionary) =>
+        !filterListsDictionary["BrandName"].IsNullOrEmpty()
+        && filterListsDictionary.Where(pair => !pair.Key.Equals("BrandName") 
+                                               && !pair.Key.Equals(
+                                                   "Category")).All(pair => pair.Value.IsNullOrEmpty())
+        && filteringModel is not ProductSearchFilteringModel;
+    
+    private IDictionary<string, int> RemoveZeroCountsFromDictionary(
+        IDictionary<string, int> countedElements) =>
+        countedElements.Where(
+                kv => kv.Value != 0).
+            ToDictionary(
+                kv => kv.Key,
+                kv => kv.Value);
+
+    private bool IsSatisfyingSearchModelPredicate(IFilteringModel filteringModel, 
+        IDictionary<string, List<string>> filterListsDictionary) =>
+        filteringModel is ProductSearchFilteringModel 
+        && filterListsDictionary["BrandName"].IsNullOrEmpty()
+        && filterListsDictionary["Category"].IsNullOrEmpty();
+    
+    private void CountSpecifications(IEnumerable<ProductSpecification> allSpecifications,
+        IDictionary<string, int> countedSpecs, 
+        IEnumerable<Product> filteredProducts,
+        IEnumerable<Product> categoryRelatedProducts)
+    {
+        foreach (var specification in allSpecifications)
+        {
+            var spec = new SpecificationDto
+            {
+                Category = specification.SpecificationCategory.Value,
+                Attribute = specification.SpecificationAttribute.Value,
+                Value = specification.SpecificationValue.Value
+            };
+
+            var count = filteredProducts.Any(product => specification.Products.Contains(product)) 
+                ? filteredProducts.Count(product => specification.Products.Contains(product))
+                : categoryRelatedProducts.Count(product => specification.Products.Contains(product));
+
+            if (!countedSpecs.Where(_ => countedSpecs.ContainsKey(spec.ToString()!)).IsNullOrEmpty())
+                continue;
+    
+            countedSpecs.Add(spec.ToString()!, count);
+        }
+    }
+}
