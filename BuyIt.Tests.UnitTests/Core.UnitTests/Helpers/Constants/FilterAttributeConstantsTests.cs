77fd2a77237c284d441d9cc6d875e26c72d91871
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-12-14 21:02:00 +0200

    Added constant tests as a part of helpers testing

---
+++ BuyIt.Tests.UnitTests/Core.UnitTests/Helpers/Constants/FilterAttributeConstantsTests.cs
@@ -0,0 +1,17 @@
+﻿using Application.Helpers.SpecificationResolver.Common.Constants;
+using Xunit;
+
+namespace BuyIt.Tests.UnitTests.Core.UnitTests.Helpers.Constants;
+
+public class FilterAttributeConstantsTests
+{
+    [Fact]
+    public void RemovedAttributes_ShouldNotBeNull()
+    {
+        var filterAttributes = new FilterAttributeConstants();
+
+        var removedAttributes = filterAttributes.RemovedAttributes;
+
+        Assert.NotNull(removedAttributes);
+    }
+}
---
+++ BuyIt.Tests.UnitTests/Core.UnitTests/Helpers/Constants/FilterCategoryConstantsTests.cs
@@ -0,0 +1,17 @@
+﻿using Application.Helpers.SpecificationResolver.Common.Constants;
+using Xunit;
+
+namespace BuyIt.Tests.UnitTests.Core.UnitTests.Helpers.Constants;
+
+public class FilterCategoryConstantsTests
+{
+    [Fact]
+    public void RemovedCategories_ShouldNotBeNull()
+    {
+        var filterCategories = new FilterCategoryConstants();
+
+        var removedCategories = filterCategories.RemovedCategories;
+
+        Assert.NotNull(removedCategories);
+    }
+}
