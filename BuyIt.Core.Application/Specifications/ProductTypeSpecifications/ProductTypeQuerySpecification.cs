dc10b2dfe82b506d27b6c8534a7ca735ad223d3c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2023-12-28 20:51:20 +0200

    Added additional constructor for ProductTypeQuerySpecification.cs

--- BuyIt.Core.Application/Specifications/ProductTypeSpecifications/ProductTypeQuerySpecification.cs
+++ BuyIt.Core.Application/Specifications/ProductTypeSpecifications/ProductTypeQuerySpecification.cs
@@ -1,9 +1,17 @@
-﻿using Application.Specifications.Common;
+﻿using System.Linq.Expressions;
+using Application.Specifications.Common;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Specifications.ProductTypeSpecifications;
 
 public sealed class ProductTypeQuerySpecification : QuerySpecification<ProductType>
 {
     public ProductTypeQuerySpecification() => AddOrderByAscending(p => p.Name);
+
+    public ProductTypeQuerySpecification(Expression<Func<ProductType, bool>> expression)
+        : base(expression)
+    {
+        Includes.Add(p => p.Include(c => c.Products));
+    }
 }
