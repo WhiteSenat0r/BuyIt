2836d155730690eef5e856cba76c0eae5c61b5b0
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-11 21:51:00 +0200

    Added identity-related data transfer objects

---
+++ BuyIt.Core.Application/DataTransferObjects/IdentityRelated/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.DataTransferObjects.IdentityRelated;
+
+public class UserDto
+{
+    public string DisplayedName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Token { get; set; }
+    
+    public IEnumerable<string> Roles { get; set; }
+}
