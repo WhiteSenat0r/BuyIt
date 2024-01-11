f7be2e013d8fbc773e7a1ab184f43a6277e55dec
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-11 21:48:51 +0200

    Added RelativePathConstructor service

---
+++ BuyIt.Infrastructure.Services/FileSystem/RelativePathConstructor.cs
@@ -0,0 +1,18 @@
+﻿namespace BuyIt.Infrastructure.Services.FileSystem;
+
+public sealed class RelativePathConstructor
+{
+    private string GetBaseDirectory()
+    {
+        var currentDomainDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        
+        var splitIndex = currentDomainDirectory.LastIndexOf("BuyIt.Domain\\") + "BuyIt.Domain\\".Length;
+        
+        return currentDomainDirectory[..splitIndex].Replace('\\', '/');
+    }
+    
+    public string CreateFilePath(string solutionRootPath)
+    {
+        return GetBaseDirectory() + solutionRootPath;
+    }
+}
