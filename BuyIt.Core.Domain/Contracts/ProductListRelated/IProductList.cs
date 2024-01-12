15a78828c03c6c201e218b6e821c9158df6aa377
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-12 15:38:22 +0200

    Added product list

---
+++ BuyIt.Core.Domain/Contracts/ProductListRelated/IProductList.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Contracts.Common;
+
+namespace Domain.Contracts.ProductListRelated;
+
+public interface IProductList<TProductItem> : IEntity<Guid>
+    where TProductItem : IProductListItem
+{
+    IEnumerable<TProductItem> Items { get; set; }
+}
---
+++ BuyIt.Core.Domain/Entities/ProductListRelated/ProductList.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Contracts.ProductListRelated;
+
+namespace Domain.Entities.ProductListRelated;
+
+public class ProductList<TProductItem> : IProductList<IProductListItem>
+    where TProductItem : IProductListItem
+{
+    public ProductList() { }
+
+    public ProductList(Guid listId) => Id = listId;
+    
+    public Guid Id { get; set; }
+
+    public IEnumerable<IProductListItem> Items { get; set; }
+}
