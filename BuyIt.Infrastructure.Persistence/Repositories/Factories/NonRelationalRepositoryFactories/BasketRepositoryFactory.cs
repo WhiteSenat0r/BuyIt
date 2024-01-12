c009d1b90177c4b77c86d6203188f9d8c03fee79
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-12 15:39:49 +0200

    Added basket, comparison and wish list repository factories

---
+++ BuyIt.Infrastructure.Persistence/Repositories/Factories/NonRelationalRepositoryFactories/BasketRepositoryFactory.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities.ProductListRelated;
+using Persistence.Repositories.Common.Classes;
+using Persistence.Repositories.Factories.NonRelationalRepositoryFactories.Common.Classes;
+using StackExchange.Redis;
+
+namespace Persistence.Repositories.Factories.NonRelationalRepositoryFactories;
+
+public class BasketRepositoryFactory : NonRelationalRepositoryFactory<ProductList<BasketItem>>
+{
+    public override GenericNonRelationalRepository<ProductList<BasketItem>> Create(
+        IConnectionMultiplexer connectionMultiplexer) => new(connectionMultiplexer);
+}
---
+++ BuyIt.Infrastructure.Persistence/Repositories/Factories/NonRelationalRepositoryFactories/Common/Classes/NonRelationalRepositoryFactory.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Contracts.ProductListRelated;
+using Persistence.Repositories.Common.Classes;
+using StackExchange.Redis;
+
+namespace Persistence.Repositories.Factories.NonRelationalRepositoryFactories.Common.Classes;
+
+public abstract class NonRelationalRepositoryFactory<TEntity> 
+    where TEntity : class, IProductList<IProductListItem>, new()
+{
+    public abstract GenericNonRelationalRepository<TEntity> Create(
+        IConnectionMultiplexer connectionMultiplexer);
+}
