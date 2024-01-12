13073ce39f90edf5936f734e9330e1d1160838c0
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-12 12:20:08 +0200

    Added User and UserRole classes

---
+++ BuyIt.Core.Domain/Entities/User.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Domain.Entities;
+
+public sealed class User : IdentityUser<Guid>
+{
+    private string _firstName;
+    private string _middleName;
+    private string _lastName;
+
+    public User() => Id = Guid.NewGuid();
+
+    [MaxLength(64)]
+    public string FirstName
+    {
+        get => _firstName;
+        set => _firstName = GetCheckedValue(value, "First name");
+    }
+    
+    [MaxLength(64)]
+    public string MiddleName
+    {
+        get => _middleName;
+        set => _middleName = GetCheckedValue(value, "Middle name");
+    }
+    
+    [MaxLength(64)]
+    public string LastName
+    {
+        get => _lastName;
+        set => _lastName = GetCheckedValue(value, "Last name");
+    }
+    
+    private string GetCheckedValue(string value, string valueName)
+    {
+        return !value.IsNullOrEmpty() 
+            ? value
+            : throw new ArgumentNullException($"{valueName} is null or empty!");
+    }
+}
---
+++ BuyIt.Core.Domain/Entities/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities;
+
+public sealed class UserRole : IdentityRole<Guid>
+{
+    public UserRole() { }
+
+    public UserRole(string roleName) : base(roleName)
+    {
+        Id = Guid.NewGuid();
+    }
+}
