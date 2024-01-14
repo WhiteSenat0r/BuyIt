76024a3681370b29e76028430aea093ac041e066
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-14 22:51:30 +0200

    Fixed bug with incorrect predicate option execution in CreateOrUpdate method in BaseFeatureController.cs

--- BuyIt.Presentation.WebAPI/Controllers/ProductRelatedControllers/FeaturesRelated/Common/BaseFeatureController.cs
+++ BuyIt.Presentation.WebAPI/Controllers/ProductRelatedControllers/FeaturesRelated/Common/BaseFeatureController.cs
@@ -16,24 +16,24 @@ namespace BuyIt.Presentation.WebAPI.Controllers.ProductRelatedControllers.Featur
             _repository = repository;
 
         [HttpGet]
-        [ProducesResponseType(typeof(IProductList<IProductListItem>), StatusCodes.Status200OK)]
-        public virtual async Task<ActionResult<IProductList<TProductListItem>>> Get(Guid listId) => 
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IProductList<TProductListItem>>> Get(Guid listId) => 
             Ok(await _repository.GetSingleEntityByIdAsync(listId));
 
-        [HttpPut("update")]
-        [ProducesResponseType(typeof(IProductList<IProductListItem>), StatusCodes.Status200OK)]
-        public async Task<ActionResult<IProductList<TProductListItem>>> Update([FromBody]
+        [HttpPost("createorupdate")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IProductList<TProductListItem>>> CreateOrUpdate([FromBody]
             ProductList<TProductListItem> list)
         {
-            var updatedList = User.Identity is not null 
-                ? await _repository.UpdateExistingEntityAsync(list)
-                : await _repository.UpdateExistingEntityAsync(list, 7);
+            var updatedList = User.Identity!.IsAuthenticated 
+                ? await _repository.CreateOrUpdateEntityAsync(list)
+                : await _repository.CreateOrUpdateEntityAsync(list, 7);
 
             return Ok(updatedList);
         }
         
         [HttpDelete("delete")]
-        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> Delete(Guid listId)
         {
             var removalResult = await _repository.RemoveExistingEntityAsync(listId);
