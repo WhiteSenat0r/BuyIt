a031b8117e89cebec0ce55741c86954b8454145d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-18 14:57:14 +0200

    Added ImageLinkValidityAttribute.cs

---
+++ BuyIt.Core.Application/Validation/Attributes/Links/ImageLinkValidityAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Application.Validation.Attributes.Links
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field 
+                                              | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class ImageLinkValidityAttribute : ValidationAttribute
+    {
+        private const string ImageLinkPattern = "\\b(?:https?):\\/\\/[^\\s\\/$.?#].[^\\s]*\\.(?:jpg)\\b";
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is not string email)
+                return new ValidationResult("The image link can not be validated!");
+
+            return !Regex.IsMatch(email, ImageLinkPattern) 
+                ? new ValidationResult( 
+                    "The link you entered is not in the correct format or has unsuitable image format!!") 
+                : ValidationResult.Success;
+        }
+    }
+}
