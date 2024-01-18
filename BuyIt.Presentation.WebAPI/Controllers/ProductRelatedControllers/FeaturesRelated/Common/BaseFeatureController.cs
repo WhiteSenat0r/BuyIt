47777692b1fc8b56c66a92139d15ca45a6df1e61
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-18 14:58:10 +0200

    Updated CreateOrUpdate method's signature in BaseFeatureController.cs

--- BuyIt.Presentation.WebAPI/Controllers/ProductRelatedControllers/FeaturesRelated/Common/BaseFeatureController.cs
+++ BuyIt.Presentation.WebAPI/Controllers/ProductRelatedControllers/FeaturesRelated/Common/BaseFeatureController.cs
@@ -1,3 +1,5 @@
+using Application.DataTransferObjects.ProductListRelated;
+using AutoMapper;
 using BuyIt.Presentation.WebAPI.Controllers.Common.Classes;
 using Domain.Contracts.ProductListRelated;
 using Domain.Contracts.RepositoryRelated.NonRelational;
@@ -6,14 +8,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BuyIt.Presentation.WebAPI.Controllers.ProductRelatedControllers.FeaturesRelated.Common
 {
-    public abstract class BaseFeatureController<TProductListItem> : BaseApiController
+    public abstract class BaseFeatureController<TProductListItem, TProductListItemDto> : BaseApiController
         where TProductListItem : class, IProductListItem, new()
+        where TProductListItemDto : class, IProductListItem, new()
     {
         private readonly INonRelationalRepository<ProductList<TProductListItem>> _repository;
+        private readonly IMapper _mapper;
 
         protected BaseFeatureController(
-            INonRelationalRepository<ProductList<TProductListItem>> repository) =>
+            INonRelationalRepository<ProductList<TProductListItem>> repository,
+            IMapper mapper)
+        {
             _repository = repository;
+            _mapper = mapper;
+        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -23,8 +31,10 @@ namespace BuyIt.Presentation.WebAPI.Controllers.ProductRelatedControllers.Featur
         [HttpPost("createorupdate")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IProductList<TProductListItem>>> CreateOrUpdate([FromBody]
-            ProductList<TProductListItem> list)
+            ProductListDto<TProductListItemDto> dtoList)
         {
+            var list = _mapper.Map<ProductListDto<TProductListItemDto>, ProductList<TProductListItem>>(dtoList);
+            
             var updatedList = User.Identity!.IsAuthenticated 
                 ? await _repository.CreateOrUpdateEntityAsync(list)
                 : await _repository.CreateOrUpdateEntityAsync(list, 7);
