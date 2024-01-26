0a2f695f76005a811b091e6150c38c6a116ced9b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-26 11:02:14 +0200

    Added Authorize attribute in WishlistController.cs

--- BuyIt.Presentation.WebAPI/Controllers/ProductRelatedControllers/FeaturesRelated/WishlistController.cs
+++ BuyIt.Presentation.WebAPI/Controllers/ProductRelatedControllers/FeaturesRelated/WishlistController.cs
@@ -3,10 +3,12 @@ using AutoMapper;
 using BuyIt.Presentation.WebAPI.Controllers.ProductRelatedControllers.FeaturesRelated.Common;
 using Domain.Contracts.RepositoryRelated.NonRelational;
 using Domain.Entities.ProductListRelated;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuyIt.Presentation.WebAPI.Controllers.ProductRelatedControllers.FeaturesRelated;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class WishlistController : BaseFeatureController<WishedItem, WishedItemDto>
