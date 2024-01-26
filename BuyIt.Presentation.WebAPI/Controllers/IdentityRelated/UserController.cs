1ec3b80c906208b2b0007e49c65878541212e7d1
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-26 11:04:02 +0200

    Implemented authentication system, performed refactoring of the existing code

--- BuyIt.Presentation.WebAPI/Controllers/IdentityRelated/UserController.cs
+++ BuyIt.Presentation.WebAPI/Controllers/IdentityRelated/UserController.cs
@@ -1,15 +1,20 @@
-﻿using System.Security.Claims;
+﻿using System.Globalization;
+using System.Security.Claims;
 using Application.DataTransferObjects.IdentityRelated;
 using Application.Responses;
 using Application.Responses.Common.Classes;
 using BuyIt.Infrastructure.Services.Mailing;
-using BuyIt.Infrastructure.Services.Mailing.Common.Classes.Options;
+using BuyIt.Infrastructure.Services.Mailing.Common.Items.MessageTemplates.MessageTemplates;
+using BuyIt.Infrastructure.Services.Mailing.Common.Items.Options;
 using BuyIt.Presentation.WebAPI.Controllers.Common.Classes;
+using Domain.Contracts.RepositoryRelated.Relational;
 using Domain.Contracts.TokenRelated;
 using Domain.Entities.IdentityRelated;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BuyIt.Presentation.WebAPI.Controllers.IdentityRelated;
 
@@ -19,14 +24,17 @@ public class UserController : BaseApiController
     private readonly SignInManager<User> _signInManager;
     private readonly RoleManager<UserRole> _roleManager;
     private readonly ITokenService _tokenService;
+    private readonly IRepository<RefreshToken> _refreshTokenRepository;
 
     public UserController(UserManager<User> userManager, SignInManager<User> signInManager,
-        RoleManager<UserRole> roleManager, ITokenService tokenService)
+        RoleManager<UserRole> roleManager, ITokenService tokenService,
+        IRepository<RefreshToken> refreshTokenRepository)
     {
         _userManager = userManager;
         _signInManager = signInManager;
         _roleManager = roleManager;
         _tokenService = tokenService;
+        _refreshTokenRepository = refreshTokenRepository;
     }
 
     [Authorize]
@@ -40,15 +48,19 @@ public class UserController : BaseApiController
 
         var user = await _userManager.FindByEmailAsync(userEmail);
         
-        return Ok(new UserDto
-        {
-            DisplayedName = $"{user.FirstName} {user.LastName}",
-            Email = user.Email,
-            Token = _tokenService.CreateToken(user),
-            Roles = await _userManager.GetRolesAsync(user)
-        });
+        return Ok(await GetUserDataResponse(user));
     }
     
+    [AllowAnonymous]
+    [HttpGet("AuthStatus")]
+    [ProducesResponseType(typeof(AuthStatusDto), StatusCodes.Status200OK)]
+    public async Task<ActionResult<UserDto>> GetCookiesStatus() =>
+        Ok(new AuthStatusDto
+        {
+            ContainsAccessToken = !Request.Cookies["UserAccessToken"].IsNullOrEmpty(),
+            ContainsRefreshToken = !Request.Cookies["UserRefreshToken"].IsNullOrEmpty(),
+        });
+
     [AllowAnonymous]
     [HttpPost("Login")]
     [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
@@ -67,26 +79,60 @@ public class UserController : BaseApiController
 
         var user = await _userManager.FindByEmailAsync(loginData.Email);
         
+        if (!user.EmailConfirmed) return Unauthorized(
+            new ApiResponse(401, "The email is not verified!"));
+        
         var passwordIsValid = await _signInManager.CheckPasswordSignInAsync(
             user, loginData.Password, false);
         
         if (!passwordIsValid.Succeeded) return Unauthorized(
             new ApiResponse(401, "Incorrect password!"));
+
+        await SetRefreshTokenAsync(user);
+        await GenerateAccessTokenAsync(user);
         
-        return Ok(new UserDto
-        {
-            DisplayedName = $"{user.FirstName} {user.LastName}",
-            Email = user.Email,
-            Token = _tokenService.CreateToken(user),
-            Roles = await _userManager.GetRolesAsync(user)
-        });
+        return Ok(await GetUserDataResponse(user));
     }
     
+    [Authorize]
+    [HttpPost("Logout")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult> Logout()
+    {
+        var user = await _userManager.Users.Include(u => u.RefreshTokens)
+            .SingleOrDefaultAsync(u => u.UserName.Equals(
+                User.FindFirstValue(ClaimTypes.Name)));
+
+        if (user is null) return Unauthorized(
+            new ApiResponse(401, "User is not authorized!"));
+
+        var refreshToken = Request.Cookies["UserRefreshToken"];
+
+        var oldToken = user.RefreshTokens.SingleOrDefault(t => t.Value.Equals(refreshToken));
+        
+        if (oldToken is not null)
+        {
+            oldToken.RevocationDate = DateTime.UtcNow;
+            
+            _refreshTokenRepository.UpdateExistingEntity(oldToken);
+        }
+
+        var invalidUserTokens = user.RefreshTokens.Where(t => !t.IsValid);
+        
+        if (!invalidUserTokens.IsNullOrEmpty())
+            _refreshTokenRepository.RemoveRangeOfExistingEntities(invalidUserTokens);
+        
+        RemoveTokenCookies();
+        
+        return Ok();
+    }
+
     [AllowAnonymous]
     [HttpPost("Register")]
-    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<UserDto>> Register([FromBody]RegistrationDto registrationData)
+    public async Task<ActionResult> Register([FromBody]RegistrationDto registrationData)
     {
         if (IsInvalidModel(out var badRequest,
                 "Error occured during registration of a new user!",
@@ -103,15 +149,9 @@ public class UserController : BaseApiController
                 Errors = new [] { "User with entered email is already registered!" }
             });
         
-        var createdUser = new User
-        {
-            FirstName = registrationData.FirstName,
-            MiddleName = registrationData.MiddleName,
-            LastName = registrationData.LastName,
-            Email = registrationData.Email,
-            PhoneNumber = registrationData.PhoneNumber,
-            UserName = $"@{registrationData.FirstName.ToLower()}{registrationData.LastName.ToLower()}"
-        };
+        var createdUser = CreateFormattedUser(registrationData);
+
+        createdUser.UserName += createdUser.Id.ToString()[..8]; 
         
         var userResult = await _userManager.CreateAsync(createdUser, registrationData.Password);
 
@@ -131,57 +171,90 @@ public class UserController : BaseApiController
             user, 
             "buyit.verify@gmail.com",
             "Registration at BuyIt!",
-            "Congratulations and welcome to BuyIt! We're excited to have you on board and thank you" +
-            " for choosing us.<br><br>Your registration process is almost complete, but before we get started," +
-            " we need to confirm your email address. Please click the button below to verify your account." +
-            "<br><br>If you are unable to click the link, you can copy and paste it into your browser's address" +
-            " bar.<br><br>Once your email address is verified, you'll gain full access to your account.<br><br>Thank" +
-            " you for joining BuyIt!",
+            EmailMessages.VerificationRequest,
             "Verify email address",
             verificationUrl);
 
-        return Ok(new UserDto
-        {
-            DisplayedName = $"{user.FirstName} {user.LastName}",
-            Email = user.Email,
-            Token = _tokenService.CreateToken(user),
-            Roles = await _userManager.GetRolesAsync(user)
-        });
+        return Ok($"New user with email {user.Email} was registered successfully!" +
+                  $" Waiting for email verification!");
     }
 
     [AllowAnonymous]
     [HttpPut("VerifyEmail")]
-    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<UserDto>> VerifyEmail(
+    public async Task<ActionResult> VerifyEmail(
         [FromQuery]string verificationToken, string email)
     {
         var user = await _userManager.FindByEmailAsync(email);
         
-        if (user is null) return BadRequest(new ApiResponse(400));
+        if (user is null) return BadRequest(new ApiResponse(
+            400, "User does not exist!"));
 
         var verificationResult = await _userManager.ConfirmEmailAsync(user, verificationToken);
         
-        if (!verificationResult.Succeeded) return BadRequest(new ApiResponse(400));
+        if (!verificationResult.Succeeded) return BadRequest(new ApiResponse(
+            400, "Email verification was not performed!"));
         
         await SendNotificationLetterAsync(
             user, 
             "buyit.verify@gmail.com",
             "Email address confirmation at BuyIt!",
-            "Congratulations! We're excited to inform you that you have successfully verified your email " +
-            "address.<br><br>Your registration process is complete and you are ready to go shopping at BuyIt!" +
-            "<br><br>We hope that our marketplace will be able to offer you everything what you need!<br><br>" +
-            "Happy purchases and enjoy your time at BuyIt!",
+            EmailMessages.SuccessfulVerification,
             null,
             null);
         
-        return Ok(new UserDto
+        return Ok($"Email ({email}) was verified successfully!");
+    }
+
+    [AllowAnonymous]
+    [HttpPut("RefreshAccessToken")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult> RefreshAccessToken()
+    {
+        var refreshToken = Request.Cookies["UserRefreshToken"];
+        
+        var user = await _userManager.Users
+            .Include(r => r.RefreshTokens)
+            .SingleOrDefaultAsync(
+                u => u.RefreshTokens.SingleOrDefault(
+                    t => t.Value.Equals(refreshToken)) != null);
+
+        if (user is null) return Unauthorized("User is not authorized!");
+
+        var oldToken = user.RefreshTokens.SingleOrDefault(
+            r => r.Value.Equals(refreshToken));
+
+        if (oldToken is not null && !oldToken.IsValid)
         {
-            DisplayedName = $"{user.FirstName} {user.LastName}",
-            Email = user.Email,
-            Token = _tokenService.CreateToken(user),
-            Roles = await _userManager.GetRolesAsync(user)
-        });
+            RemoveTokenCookies();
+
+            return Unauthorized("Refresh token is expired!");
+        }
+
+        if (oldToken is not null)
+        {
+            oldToken.RevocationDate = DateTime.UtcNow;
+            
+            _refreshTokenRepository.UpdateExistingEntity(oldToken);
+        }
+
+        var invalidUserTokens = user.RefreshTokens.Where(t => !t.IsValid);
+        
+        if (!invalidUserTokens.IsNullOrEmpty())
+            _refreshTokenRepository.RemoveRangeOfExistingEntities(invalidUserTokens);
+        
+        await GenerateAccessTokenAsync(user);
+        await SetRefreshTokenAsync(user);
+        
+        return Ok();
+    }
+
+    private void RemoveTokenCookies()
+    {
+        Response.Cookies.Delete("UserAccessToken");
+        Response.Cookies.Delete("UserRefreshToken");
     }
 
     [AllowAnonymous]
@@ -211,6 +284,63 @@ public class UserController : BaseApiController
         return false;
     }
 
+    private User CreateFormattedUser(RegistrationDto registrationData) =>
+        new()
+        {
+            FirstName = new CultureInfo("en-US").TextInfo.ToTitleCase(registrationData.FirstName),
+            MiddleName = new CultureInfo("en-US").TextInfo.ToTitleCase(registrationData.MiddleName),
+            LastName = new CultureInfo("en-US").TextInfo.ToTitleCase(registrationData.LastName),
+            Email = registrationData.Email,
+            PhoneNumber = registrationData.PhoneNumber,
+            UserName = $"@{registrationData.FirstName.ToLower()}{registrationData.LastName.ToLower()}-"
+        };
+    
+    private async Task<UserDto> GetUserDataResponse(User user) =>
+        new()
+        {
+            DisplayedName = $"{user.FirstName} {user.LastName}",
+            Email = user.Email,
+            Roles = await _userManager.GetRolesAsync(user)
+        };
+    
+    private async Task SetRefreshTokenAsync(User user)
+    {
+        var refreshToken = _tokenService.CreateRefreshToken();
+        refreshToken.UserId = user.Id;
+        await _refreshTokenRepository.AddNewEntityAsync(refreshToken);
+        
+        user.RefreshTokens.Add(refreshToken);
+        
+        await _userManager.UpdateAsync(user);
+
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.None,
+            Expires = refreshToken.ExpiryDate
+        };
+        
+        Response.Cookies.Append(
+            "UserRefreshToken", refreshToken.Value, cookieOptions);
+    }
+    
+    private async Task GenerateAccessTokenAsync(User user)
+    {
+        var accessToken = _tokenService.CreateAccessToken(user);
+
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.None,
+            Expires = DateTime.UtcNow.AddMinutes(5)
+        };
+        
+        Response.Cookies.Append(
+            "UserAccessToken", accessToken, cookieOptions);
+    }
+    
     private async Task SendNotificationLetterAsync(User user, string senderEmail,
         string subject, string message, string buttonName, string buttonUrl)
     {
