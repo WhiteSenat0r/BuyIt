bc6974060507bb211c56bed1e7d683fa3e13781c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-26 11:00:05 +0200

    Added RefreshTokenQuerySpecification.cs

---
+++ BuyIt.Core.Application/Specifications/Identity/RefreshTokenQuerySpecification.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using Application.Specifications.Common;
+using Domain.Entities.IdentityRelated;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Specifications.Identity;
+
+public sealed class RefreshTokenQuerySpecification : QuerySpecification<RefreshToken>
+{
+    public RefreshTokenQuerySpecification() => 
+        Includes.Add(refreshToken => refreshToken.Include(u => u.User));
+
+    public RefreshTokenQuerySpecification(
+        Expression<Func<RefreshToken, bool>> criteria) : base(criteria) =>
+        Includes.Add(refreshToken => refreshToken.Include(u => u.User));
+}
