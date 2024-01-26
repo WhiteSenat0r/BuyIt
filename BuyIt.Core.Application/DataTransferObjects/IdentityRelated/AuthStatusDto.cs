4f28ea794713580a83e4782d1753a9926f66bdc0
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-26 11:04:19 +0200

    Added AuthStatusDto.cs

---
+++ BuyIt.Core.Application/DataTransferObjects/IdentityRelated/AuthStatusDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DataTransferObjects.IdentityRelated;
+
+public class AuthStatusDto
+{
+    public bool ContainsAccessToken  { get; set; }
+    
+    public bool ContainsRefreshToken { get; set; }
+}
