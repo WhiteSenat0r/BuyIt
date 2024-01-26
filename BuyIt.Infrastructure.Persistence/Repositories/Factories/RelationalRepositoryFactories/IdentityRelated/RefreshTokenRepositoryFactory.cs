b64e2aaac857e2fec12ce7b5fc2ce252adefb273
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-26 11:01:03 +0200

    Added RefreshTokenRepository.cs and RefreshTokenRepositoryFactory.cs

---
+++ BuyIt.Infrastructure.Persistence/Repositories/Factories/RelationalRepositoryFactories/IdentityRelated/RefreshTokenRepositoryFactory.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.IdentityRelated;
+using Persistence.Contexts;
+using Persistence.Repositories.Factories.RelationalRepositoryFactories.Common.Classes;
+using Persistence.Repositories.IdentityRelatedRepositories;
+
+namespace Persistence.Repositories.Factories.RelationalRepositoryFactories.IdentityRelated;
+
+public sealed class RefreshTokenRepositoryFactory : RepositoryFactory<RefreshToken>
+{
+    public override RefreshTokenRepository Create(StoreContext dbContext) => new(dbContext);
+}
---
+++ BuyIt.Infrastructure.Persistence/Repositories/IdentityRelatedRepositories/RefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.IdentityRelated;
+using Persistence.Contexts;
+using Persistence.Repositories.Common.Classes;
+
+namespace Persistence.Repositories.IdentityRelatedRepositories;
+
+public sealed class RefreshTokenRepository : GenericRepository<RefreshToken>
+{
+    internal RefreshTokenRepository
+        (StoreContext dbContext) : base(dbContext) => Context = dbContext;
+}
