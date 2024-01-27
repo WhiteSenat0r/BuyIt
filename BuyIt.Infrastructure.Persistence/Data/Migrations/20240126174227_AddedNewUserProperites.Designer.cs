16aad4b146a85a8e5bf5b978e6bf8689e34eb906
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-01-27 22:40:42 +0200

    Applied new migration

--- BuyIt.Infrastructure.Persistence/Data/Migrations/20240126174227_AddedNewUserProperites.Designer.cs
+++ BuyIt.Infrastructure.Persistence/Data/Migrations/20240126174227_AddedNewUserProperites.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Contexts;
 
@@ -11,9 +12,11 @@ using Persistence.Contexts;
 namespace Persistence.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240126174227_AddedNewUserProperites")]
+    partial class AddedNewUserProperites
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BuyIt.Infrastructure.Persistence/Data/Migrations/20240126174227_AddedNewUserProperites.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewUserProperites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BasketId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ComparisonListId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "WishListId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BasketId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ComparisonListId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WishListId",
+                table: "AspNetUsers");
+        }
+    }
+}
