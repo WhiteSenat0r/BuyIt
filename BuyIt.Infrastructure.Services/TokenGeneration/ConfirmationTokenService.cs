631d820579d5f5957e1b6c1c57350dbd7029e762
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-03 18:56:21 +0200

    Added ConfirmationTokenService.cs

---
+++ BuyIt.Infrastructure.Services/TokenGeneration/ConfirmationTokenService.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Domain.Contracts.TokenRelated;
+using Domain.Entities.IdentityRelated;
+
+namespace BuyIt.Infrastructure.Services.TokenGeneration;
+
+public sealed class ConfirmationTokenService : IConfirmationTokenService
+{
+    public string CreateToken(User user)
+    {
+        var tokenParts = new List<string>
+        {
+            Convert.ToBase64String(user.Id.ToByteArray()),
+            Convert.ToBase64String(Encoding.UTF8.GetBytes(user.UserName!)),
+            Convert.ToBase64String(Encoding.UTF8.GetBytes(user.Email!))
+        };
+
+        return tokenParts.Aggregate(
+            (currentElement, nextElement) => currentElement + "$" + nextElement)
+            .Replace('+', '*');
+    }
+
+    public bool IsValidConfirmationToken(User user, string token) => 
+        CreateToken(user).Equals(token);
+}
