965c75eacdc38fa2da1210a92a1b3d5d781848d9
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-07 21:18:20 +0200

    Added ExternalServicesExtensions.cs

---
+++ BuyIt.Infrastructure.Services/Extensions/ExternalServicesExtensions.cs
@@ -0,0 +1,28 @@
+﻿using BuyIt.Infrastructure.Services.Mailing;
+using BuyIt.Infrastructure.Services.TokenGeneration;
+using Domain.Contracts.ServiceRelated;
+using Domain.Contracts.TokenRelated;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuyIt.Infrastructure.Services.Extensions;
+
+public static class ExternalServicesExtensions
+{
+    public static IServiceCollection AddRequiredExternalServiceCollection
+        (this IServiceCollection serviceCollection) 
+        // Method that contains all services that will be used in application building process.
+        // Additional services can be added in this method in the future.
+        // Altering or removal of services can be performed at your own risk.
+    {
+        AddRequiredServices(serviceCollection);
+        
+        return serviceCollection;
+    }
+    
+    private static void AddRequiredServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<IConfirmationTokenService, ConfirmationTokenService>();
+        serviceCollection.AddScoped<IAuthenticationTokenService, AuthenticationTokenService>();
+        serviceCollection.AddScoped<IMailService, MailSender>();
+    }
+}
